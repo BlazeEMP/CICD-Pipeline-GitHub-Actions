287536109979091d08893c45ff985e049c1ee95e
BlazeEMP <blazelitanui@gmail.com>
2025-02-28 14:36:45 -0600

    adding working eslint and fixed errors

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,19 +1,15 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
 
-export default async (modelName: "Question" , collectionName: string) => {
-  try {
-    if (!models[modelName].db.db) {
-      throw new Error(`Model or database for ${modelName} does not exist`);
-    }
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray();
+export default async (modelName: "Question", collectionName: string) => {
+  if (!models[modelName].db.db) {
+    throw new Error(`Model or database for ${modelName} does not exist`);
+  }
+  const modelExists = await models[modelName].db.db.listCollections({
+    name: collectionName
+  }).toArray();
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
-    }
-  } catch (err) {
-    throw err;
+  if (modelExists.length) {
+    await db.dropCollection(collectionName);
   }
 }
